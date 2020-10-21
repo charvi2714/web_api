a68fe9f996023e629f7417b20a27409c9fc58f54
charvi2714 <121823604030@gitam.in>
2020-10-21 18:02:04 +0530

    api-testing

--- application.py
+++ application.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
+import json
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import itertools
+import os
+import uuid
+import boto3
+from botocore.client import Config
+import flask
 
 application = app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    x = {'userId': 1}
+    return jsonify(x)
+
 
 if __name__ == '__main__':
     app.run()
